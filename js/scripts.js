3cde7accc64fae828313ef04fb8db79173f82914
austinb847 <austinb847@icloud.com>
2020-04-10 11:35:35 -0700

    add calcPrice method to pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,22 @@ var Pizza = function(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 };
+
+Pizza.prototype.calcPrice = function() {
+  if (this.size === "small") {
+    this.price_size = 8.0;
+  } else if (this.size === "medium") {
+    this.price_size = 10.00;
+  } else {
+    this.price_size = 14.00;
+  }
+
+  this.price_toppings = this.toppings.length > 3 ? 5.74 : 3.50;
+
+  return this.price_size + this.price_toppings;
+
+}
+
+
+var pizza = new Pizza("medium", ["pepperoni", "olives", "mushrooms", "peppers"]);
+console.log(pizza.calcPrice());
