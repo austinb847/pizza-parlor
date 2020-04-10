0a33a416af8797ca47aba36288ba166c697ad35a
austinb847 <austinb847@icloud.com>
2020-04-10 14:47:44 -0700

    get values from form, store toppings in an array

--- js/scripts.js
+++ js/scripts.js
@@ -20,5 +20,35 @@ Pizza.prototype.calcPrice = function() {
 }
 
 
-var pizza = new Pizza("medium", ["pepperoni", "olives", "mushrooms", "peppers"]);
+/* var pizza = new Pizza("medium", ["pepperoni", "olives", "mushrooms", "peppers"]);
 console.log(pizza.calcPrice());
+console.log(pizza.price_size);
+console.log(pizza.price_toppings); */
+
+
+//UI logic
+
+$(document).ready(function() {
+
+  var createToppingsArray = function(topping_selections) {
+    var toppings = [];
+    topping_selections.each(function() {
+      toppings.push(this.value);
+    });
+    return toppings
+  };
+
+  $("form#orderForm").submit(function(event) {
+    event.preventDefault();
+    var size_selection = $("#pizza-size :selected").val();
+    var topping_selections = $("input[name='toppings_select']:checked");
+    var topping_array = createToppingsArray(topping_selections);
+    
+    var pizza = new Pizza(size_selection, topping_array);
+    var price = pizza.calcPrice();
+    console.log(price);
+    /* $("form#orderForm").hide();
+    $(".itemPanel").show();
+    $(".imgContainer").show(); */
+  });
+});
